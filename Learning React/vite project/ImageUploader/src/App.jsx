94fb51c9517eb6a0cd22a9c88cf77de485531e0c
vmhowley <vmhowleyh@gmail.com>
2023-08-29 09:29:06 -0400

    all projects

---
+++ Learning React/vite project/ImageUploader/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './navbar'
+
+export default function App() {
+
+  return (
+<>
+  <Navbar />
+  <div>
+  </div>
+
+</>
+  )
+}
+
+
---
+++ Learning React/vite project/ImageUploader/src/navbar.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+export default function Navbar(){
+    return(
+        <nav className='nav'> 
+        <a href="/">WindBNB
+        </a>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a  href="/">About Us</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+
+      </ul>
+        
+    </nav>
+    )
+}
